8e827c1438267673f6d243bb4dbff3fc60b9e2ce
stephinwv <thekingdomseeker@aol.com>
2017-08-18 18:10:06 -0400

    Add files via upload
    Just got home after a long drive...ugh. got the toppings into an array, now have no implicit conversion of Array into String
    file: app.rb location: + line: 12. Still plugging away

--- app.rb
+++ app.rb
@@ -1,13 +1,22 @@
 require "sinatra"
 require_relative "make_order.rb" 
 get '/' do 
-erb	:sandwich_parts, :locals=>{:toppings=>toppings}
+# erb	:sandwich_parts
 end
 post '/sandwich_parts' do
+	puts "MADE IT TO POST SANDWICH PARTS DO"
+	# order = order(toppings)
 	toppings = params[:toppings]
-	redirect'/order?toppings='+toppings
+	# toppings.to_s.flatten!
+	puts "toppings = #{toppings}"
+	redirect'/order?toppings=' + toppings
 end
 post '/order' do
+	puts "MADE IT TO POST ORDER DO"
+	# order = order(toppings)
 	toppings = params[:toppings]
+	# toppings = toppings.to_s.flatten!
+	puts "MADE IT TO POST ORDER DO"
+	puts "TOPPINGS + #{toppings}"
 	erb :order, :locals=>{:toppings=>toppings}
 end
