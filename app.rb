b240911f0ff502f4201e2b4f5d320402ad0c220e
stephinwv <thekingdomseeker@aol.com>
2017-08-18 22:09:54 -0400

    Add files via upload
    Made it to results/confirmation page, but lost the values. Looking for typos....there have been many.

--- app.rb
+++ app.rb
@@ -4,46 +4,28 @@ get '/' do
 erb	:sandwich_parts
 end
 post '/sandwich_parts' do
-	puts "MADE IT TO POST SANDWICH PARTS DO"
-	# order = order(toppings)
-	toppings = params[:toppings].to_s
 
-	puts "toppings = #{toppings}"
+	toppings = params[:toppings].to_s
 	redirect'/order?toppings=' + toppings
 end
 get '/order' do
-	puts "MADE IT TO gt ORDER DO"
-	# order = order(toppings)
-	toppings = params[:toppings].split
-	
 
-	puts "TOPPINGS + #{toppings}"
+	toppings = params[:toppings].split
 	erb :order, :locals=>{:toppings=>toppings}
 end
 post '/order' do
-	puts "MADE IT TO POST ORDER DO"
-	# order = order(toppings)
 	toppings = params[:toppings].to_s
-	
-	
-	puts "TOPPINGS + #{toppings}"
-	redirect '/confirmation?toppings=' + toppings
+	redirect '/confimation?toppings=' + toppings
 end
-get  '/confirmation'do
-puts "MADE IT TO get confimation DO"
-	# order = order(toppings)
+get  '/confimation'do
+puts "MADE IT TO post confimation DO"
 	toppings = params[:toppings].split
-	
-	
 	puts "TOPPINGS + #{toppings}"
-	erb :order, :locals=>{:toppings=>toppings}
+	erb :confimation, :locals=>{:toppings=>toppings}
 end
-post '/confirmation'do
-puts "MADE IT TO post confirmation DO"
-	# ORDERder = order(toppings)
-	
+post '/confimation'do
+puts "MADE IT TO post confimation DO"
 	toppings = params[:toppings].to_s
-	
 	puts "TOPPINGS + #{toppings}"
-	erb :order, :locals=>{:toppings=>toppings}
+	erb :confimation, :locals=>{:toppings=>toppings}
 end
