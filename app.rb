bd091b9b3caedb1599e55925cc5638107aa91da9
stephinwv <thekingdomseeker@aol.com>
2017-08-18 21:26:56 -0400

    Add files via upload
    Got check boxes on confirmation page beside each item, a few kinks to iron out.

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require "sinatra"
-require_relative "make_order.rb" 
+# require_relative "make_order.rb" 
 get '/' do 
 erb	:sandwich_parts
 end
@@ -14,36 +14,36 @@ end
 get '/order' do
 	puts "MADE IT TO gt ORDER DO"
 	# order = order(toppings)
-	toppings = params[:toppings].to_s
+	toppings = params[:toppings].split
+	
 
-	puts "MADE IT TO POST ORDER DO"
 	puts "TOPPINGS + #{toppings}"
 	erb :order, :locals=>{:toppings=>toppings}
 end
 post '/order' do
 	puts "MADE IT TO POST ORDER DO"
 	# order = order(toppings)
-	toppings = params[:toppings]
-	toppings = toppings.to_s
-	puts "MADE IT TO POST ORDER DO"
+	toppings = params[:toppings].to_s
+	
+	
 	puts "TOPPINGS + #{toppings}"
-	redirect '/confirmation?toppings' + toppings
+	redirect '/confirmation?toppings=' + toppings
 end
 get  '/confirmation'do
-puts "MADE IT TO post confimation DO"
+puts "MADE IT TO get confimation DO"
 	# order = order(toppings)
-	toppings = params[:toppings].to_s
-
-	puts "MADE IT TO POST ORDER DO"
+	toppings = params[:toppings].split
+	
+	
 	puts "TOPPINGS + #{toppings}"
 	erb :order, :locals=>{:toppings=>toppings}
 end
 post '/confirmation'do
-puts "MADE IT TO post confimation DO"
-	# order = order(toppings)
+puts "MADE IT TO post confirmation DO"
+	# ORDERder = order(toppings)
+	
 	toppings = params[:toppings].to_s
-
-	puts "MADE IT TO POST ORDER DO"
+	
 	puts "TOPPINGS + #{toppings}"
 	erb :order, :locals=>{:toppings=>toppings}
 end
