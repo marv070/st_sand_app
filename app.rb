6415c5e454ba0bada6fac40c123292ec408a84a1
stephinwv <thekingdomseeker@aol.com>
2017-08-20 15:56:24 -0400

    Add files via upload
    got rid of brackets, quotes, commas, etc on variables leading up to confimation page, but the values are still in one big array and are displaying quotes. I am beyond frustrated trying to figure this out. It seems so simple and straight forward, but for some reason it has left me bumfuzzled.

--- app.rb
+++ app.rb
@@ -1,34 +1,39 @@
 require "sinatra"
-require_relative "make_order.rb" 
+
 get '/' do 
 erb	:sandwich_parts
 end
-post '/sandwich_parts' do
 
-	toppings = params[:toppings].to_s
-	redirect'/order?toppings=' + toppings
+post '/sandwich_parts' do
+    # toppings = params[:toppings].to_s
+   	toppings = params[:toppings].join(',')
+    # p "what does toppings.to_s look like: #{toppings}"
+    # p "what if instead i use toppings.join(',') #{toppings_with_join_method}"
+    redirect'/order?toppings=' + toppings
 end
+
 get '/order' do
 
-	toppings = params[:toppings].split
+	toppings = params[:toppings].split(', ')
 	erb :order, :locals=> {:toppings => toppings}
 end
 post '/order' do
-	toppings = params[:toppings].to_s
-	value = params[:value].to_s
-	redirect '/confimation?toppings=' + toppings + '&value=' + value
+	toppings = params[:toppings].join(', ')
+	# value = params[:value].join(', ')
+	redirect '/confimation?toppings=' + toppings 
 end
 get  '/confimation'do
 puts "MADE IT TO get confimation DO"
-	toppings = params[:toppings].split
-	puts "TOPPINGS + #{toppings}"
-	value = params[:value].split
-	erb :confimation, :locals => {:toppings => toppings, :value=>value}
+	toppings = params[:toppings].split(', ')
+	puts " get TOPPINGS + #{toppings}"
+	# value = params[:value].split
+	erb :confimation, :locals => {:toppings => toppings}
 end
 post '/confimation'do
 puts "MADE IT TO post confimation DO"
-	toppings = params[:toppings].to_s
-	puts "TOPPINGS + #{toppings}"
-	value = params[:value].to_s
-	erb :confimation, :locals=>{:toppings=>toppings, :value=>value}
+	toppings = params[:toppings].join(', ')
+
+	puts " post TOPPINGS + #{toppings}"
+	
+	erb :confimation, :locals=>{:toppings=>toppings}
 end
